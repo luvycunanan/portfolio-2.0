b252c867ad4ebe6e348a704b85d65fc731079871
luvycunanan <luvy.cun@gmail.com>
2022-02-01 23:44:15 -0800

    adds dark mode functionality but bugs out

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const btn = document.querySelector('button');
-function sanityCheck() {
-  alert("Button Clicked!")
-}
+const toggleDarkModeBtn = document.querySelector('#dark-mode-icon');
+
+toggleDarkModeBtn.addEventListener('click', () => {
+  if (!document.documentElement.hasAttribute('data-theme', 'dark')) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+
+  console.log('clicked')
+});
